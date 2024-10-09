27ccbb40736b3fcfe171e40714775f3094458664
developermartins <martssilvapro@gmail.com>
2024-10-09 17:47:31 -0300

    add new colors

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -2,7 +2,9 @@
 
 const UserCard = ({ type }:{ type:string }) => {
   return (
-    <div>UserCard</div>
+    <div className="rounded-2xl odd:bg-martinsPurple even:bg-martinsYellow p-4 flex-1">
+      UserCard
+    </div>
   );
 };
 
